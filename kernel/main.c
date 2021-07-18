1c042e05a36b4afd0c8a2cd623f2dd0dfe621d6b
Lennard Walter <lennardwalter@gmx.de>
2021-07-18 14:55:20 +0200

    implement proper terminal printing

--- kernel/main.c
+++ kernel/main.c
@@ -1,14 +1,9 @@
 #include "multiboot.h"
+#include "terminal.h"
 #include "types.h"
 
-void write_string(const char* string, uint8_t style) {
-    uint8_t* vmem = (uint8_t*)0xB8000;
-    while (*string != 0) {
-        *vmem++ = *string++;
-        *vmem++ = style;
-    }
-}
-
 void kmain(multiboot_info_t* mb_info) {
-    write_string("Hello, there !", 15);
+    terminal_init(mb_info->framebuffer_width, mb_info->framebuffer_height);
+    terminal_set_style(TERMINAL_COLOR_RED, TERMINAL_COLOR_BLACK);
+    terminal_puts("Hello\nthere\n!");
 };
