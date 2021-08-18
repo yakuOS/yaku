1fb403374d0f6254b89be92851a79ebf8782f694
blacklyger <git@blacklyger.com>
2021-08-18 21:01:21 +0200

    Set up Long Mode

---
+++ kernel/main64.asm
@@ -0,0 +1,21 @@
+global long_mode_start
+extern kmain
+
+section .text
+bits 64
+long_mode_start:
+
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    call kmain
+
+    ;Print "OKAY" to screen
+    ;mov rax, 0x2f592f412f4b2f4f
+    ;mov qword [0xB8000], rax
+
+    hlt
