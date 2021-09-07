ff94b4c00443b8e91a4dd09001e63708f246013a
Lennard Walter <lennardwalter@gmx.de>
2021-09-07 13:06:02 +0200

    add string formatting functions

--- kernel/main.c
+++ kernel/main.c
@@ -1,3 +1,5 @@
+#include <printf.h>
+
 #include "multiboot.h"
 #include "terminal.h"
 #include "types.h"
@@ -5,5 +7,7 @@
 void kmain(multiboot_info_t* mb_info) {
     terminal_init(mb_info->framebuffer_width, mb_info->framebuffer_height);
     terminal_set_style(TERMINAL_COLOR_RED, TERMINAL_COLOR_BLACK);
-    terminal_puts("Hello\nthere\n!");
+    char buffer[20];
+    sprintf(buffer, "Hello, %s!", "there");
+    terminal_puts(buffer);
 };
