6691d30e5a397f204b787c26f499c3e4e87d889d
Timon Gärtner <39493421+TimonGaertner@users.noreply.github.com>
2021-10-05 23:33:02 +0200

    init vga_text before the rest
    makes sense for debugging, before you couldn't print e.g. error messages on the screen while initializing things

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -8,17 +8,18 @@
 #include <types.h>
 
 void kernel_main(multiboot_info_t* mb_info) {
-    pic_init();
-    idt_init();
-    pit_init(60);
     vga_text_init(mb_info->framebuffer_width, mb_info->framebuffer_height);
 
     vga_text_set_style(VGA_TEXT_COLOR_RED, VGA_TEXT_COLOR_BLACK);
-    
+
+    pic_init();
+    idt_init();
+    pit_init(60);
+
     char buffer[20];
     snprintf(buffer, 20, "Hello, %s!\n", "there");
     vga_text_puts(buffer);
-
+    
     for (;;) {
         asm("hlt");
     }
