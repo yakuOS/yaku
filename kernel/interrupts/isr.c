ef0a60831004853b956aa08adc66e6f6b53180e7
Lennard Walter <lennardwalter@gmx.de>
2021-10-07 11:22:45 +0200

    rename isr_xframe to isr_context

--- kernel/interrupts/isr.c
+++ kernel/interrupts/isr.c
@@ -42,22 +42,22 @@ static const char* exception_message(uint64_t vector_number) {
     }
 }
 
-void isr_exception_handler(isr_xframe_t* frame) {
+void isr_exception_handler(isr_context_t* ctx) {
 
-    const char* exception_msg = exception_message(frame->base_frame.vector);
+    const char* exception_msg = exception_message(ctx->base_frame.vector);
 
     char buffer[50];
     snprintf(buffer, 50, "EXCEPTION: %s (%llu, %llu)\n", exception_msg,
-             frame->base_frame.vector, frame->base_frame.error_code);
+             ctx->base_frame.vector, ctx->base_frame.error_code);
     vga_text_puts(buffer);
     asm("cli; hlt");
 }
 
-void isr_irq0(isr_xframe_t* frame) {
+void isr_irq0(isr_context_t* ctx) {
     pit_tick_increment();
 }
 
-void isr_irq1(isr_xframe_t* frame) {
+void isr_irq1(isr_context_t* ctx) {
     uint8_t scan_code = io_inb(0x60);
 
     char buffer[15];
@@ -65,17 +65,17 @@ void isr_irq1(isr_xframe_t* frame) {
     vga_text_puts(buffer);
 }
 
-void isr_irq2(isr_xframe_t* frame) {}
-void isr_irq3(isr_xframe_t* frame) {}
-void isr_irq4(isr_xframe_t* frame) {}
-void isr_irq5(isr_xframe_t* frame) {}
-void isr_irq6(isr_xframe_t* frame) {}
-void isr_irq7(isr_xframe_t* frame) {}
-void isr_irq8(isr_xframe_t* frame) {}
-void isr_irq9(isr_xframe_t* frame) {}
-void isr_irq10(isr_xframe_t* frame) {}
-void isr_irq11(isr_xframe_t* frame) {}
-void isr_irq12(isr_xframe_t* frame) {}
-void isr_irq13(isr_xframe_t* frame) {}
-void isr_irq14(isr_xframe_t* frame) {}
-void isr_irq15(isr_xframe_t* frame) {}
+void isr_irq2(isr_context_t* ctx) {}
+void isr_irq3(isr_context_t* ctx) {}
+void isr_irq4(isr_context_t* ctx) {}
+void isr_irq5(isr_context_t* ctx) {}
+void isr_irq6(isr_context_t* ctx) {}
+void isr_irq7(isr_context_t* ctx) {}
+void isr_irq8(isr_context_t* ctx) {}
+void isr_irq9(isr_context_t* ctx) {}
+void isr_irq10(isr_context_t* ctx) {}
+void isr_irq11(isr_context_t* ctx) {}
+void isr_irq12(isr_context_t* ctx) {}
+void isr_irq13(isr_context_t* ctx) {}
+void isr_irq14(isr_context_t* ctx) {}
+void isr_irq15(isr_context_t* ctx) {}
