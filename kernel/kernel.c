ace2d87957b2a04115efca0de6c96550c0a243b2
Timon Gärtner <39493421+TimonGaertner@users.noreply.github.com>
2021-10-07 11:16:22 +0200

    init vga_text before the rest (#7)
    before this change, it was not possible to print to the screen while initializing other drivers

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -9,18 +9,18 @@
 #include <types.h>
 
 void kernel_main(multiboot_info_t* mb_info) {
+    vga_text_init(mb_info->framebuffer_width, mb_info->framebuffer_height);
+    vga_text_set_style(VGA_TEXT_COLOR_RED, VGA_TEXT_COLOR_BLACK);
+
     pic_init();
     idt_init();
     pit_init(60);
     ps2_init();
-    vga_text_init(mb_info->framebuffer_width, mb_info->framebuffer_height);
-
-    vga_text_set_style(VGA_TEXT_COLOR_RED, VGA_TEXT_COLOR_BLACK);
 
     char buffer[20];
     snprintf(buffer, 20, "Hello, %s!\n", "there");
     vga_text_puts(buffer);
-
+    
     for (;;) {
         asm("hlt");
     }
