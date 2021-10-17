fa3b9e95158694a91fce52cf131729eda94ea0fe
Timon Gärtner <39493421+TimonGaertner@users.noreply.github.com>
2021-10-17 04:06:21 +0200

    Update idt.asm

--- kernel/interrupts/idt.asm
+++ kernel/interrupts/idt.asm
@@ -1,3 +1,5 @@
+; this snippet was copied from:
+; rizet; skylight; https://github.com/rizet/skylight/blob/trunk/glass/src/cpu/interrupts/idt.s, 10.09.2021
 global idt_reload
 idt_reload:
     push rbp
