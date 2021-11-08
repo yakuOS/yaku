8b718bf0858d524f880107f16d7395dd990f3f9b
Lennard Walter <lennardwalter@gmx.de>
2021-11-08 19:52:06 +0100

    implement vfctprintf for thirdparty/printf

--- kernel/thirdparty/printf/printf.c
+++ kernel/thirdparty/printf/printf.c
@@ -876,12 +876,24 @@ int vsnprintf_(char* buffer, size_t count, const char* format, va_list va)
 }
 
 
+static int _vfctprintf(void (*out)(char character, void* arg), void* arg, const char* format, va_list va)
+{
+  const out_fct_wrap_type out_fct_wrap = { out, arg };
+  return _vsnprintf(_out_fct, (char*)(uintptr_t)&out_fct_wrap, (size_t)-1, format, va);
+}
+
+
 int fctprintf(void (*out)(char character, void* arg), void* arg, const char* format, ...)
 {
   va_list va;
   va_start(va, format);
-  const out_fct_wrap_type out_fct_wrap = { out, arg };
-  const int ret = _vsnprintf(_out_fct, (char*)(uintptr_t)&out_fct_wrap, (size_t)-1, format, va);
+  const int ret = _vfctprintf(out, arg, format, va);
   va_end(va);
   return ret;
+}
+
+
+int vfctprintf(void (*out)(char character, void* arg), void* arg, const char* format, va_list va)
+{
+  return _vfctprintf(out, arg, format, va);
 }
