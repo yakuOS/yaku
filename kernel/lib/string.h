8313173261a51370db2357076da842d86dd2454a
Timon Gärtner <39493421+TimonGaertner@users.noreply.github.com>
2022-01-04 22:54:50 +0100

    Delete string.h

--- kernel/lib/string.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <types.h>
-
-void strcat_inbetween(char* buffer, const char* s1, const char* s2, size_t index);
-void str_remove_inbetween(char* buffer, const char* str1, size_t index);
