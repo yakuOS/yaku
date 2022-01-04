127f3112fe4efa39f72a757ce1e7a4018dce80ce
Timon Gärtner <39493421+TimonGaertner@users.noreply.github.com>
2022-01-04 22:54:29 +0100

    move custom string functions here

--- kernel/lib/input/input_to_text.c
+++ kernel/lib/input/input_to_text.c
@@ -1,7 +1,6 @@
 #include "input_to_text.h"
 
 #include <lib/input/read_input.h>
-#include <lib/string.h>
 #include <resources/keyboard_stringmap.h>
 #include <string.h>
 #include <types.h>
@@ -31,6 +30,46 @@ bool function_key = false; // next key is a key like: alt_gr, delete, and other 
 uint8_t keycount = 0;
 
 uint32_t cursor = 0;
+
+/**
+ * dont use same pointer for buffer and another string, copy it before using
+ * @buffer: buffer to store the new string in. (minimal length: len(str1)+len(str2)+1)
+ */
+void strcat_inbetween(char* buffer, const char* str1, const char* str2, size_t index) {
+    if (index >= strlen(str1)) {
+        strcpy(buffer, str1);
+        buffer[strlen(str1)] = '\0';
+        strcat(buffer, str2);
+        return;
+    }
+    if (index < 0) {
+        index = 0;
+    }
+    strncpy(buffer, str1, index);
+    buffer[index] = '\0';
+    strcat(buffer, str2);
+    strcat(buffer, str1 + index);
+}
+
+void str_remove_inbetween(char* buffer, const char* str1, size_t index) {
+    uint16_t len = strlen(str1);
+    if (len <= 0) {
+        strcpy(buffer, str1);
+        return;
+    }
+    if (index >= len) {
+        index = len;
+        strncpy(buffer, str1, index - 2);
+        buffer[index - 1] = '\0';
+        return;
+    }
+    if (index < 0) {
+        index = 0;
+    }
+    strncpy(buffer, str1, index);
+    buffer[index] = '\0';
+    strcat(buffer, str1 + index + 1);
+}
 
 void input_to_text_handle_keystroke(char keystroke, read_input_listener* this) {
     if (keystroke <= 0) {
