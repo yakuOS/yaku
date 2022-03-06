6db303aa5caae7016201213a8ce94c146a3eb5ba
Lennard Walter <67687452+lennardwalter@users.noreply.github.com>
2022-03-06 18:18:34 +0100

    switch to stivale boot protocol (#18)
    bootloader: limine

--- make_iso.sh
+++ make_iso.sh
@@ -5,9 +5,18 @@ OUTPUT=$1
 KERNEL_BIN=$2
 PRIVATE_DIR=$3
 SRC_ROOT=$4
+LIMINE_DIR=$5
 
 
-mkdir -p ${PRIVATE_DIR}/boot/grub
-cp ${SRC_ROOT}/grub.cfg ${PRIVATE_DIR}/boot/grub
-cp ${KERNEL_BIN} ${PRIVATE_DIR}/boot
-grub-mkrescue -o ${OUTPUT} ${PRIVATE_DIR}
+mkdir -p ${PRIVATE_DIR}
+cp ${SRC_ROOT}/limine.cfg ${KERNEL_BIN} ${LIMINE_DIR}/limine-cd.bin \
+    ${LIMINE_DIR}/limine.sys ${LIMINE_DIR}/limine-eltorito-efi.bin \
+    ${PRIVATE_DIR}
+
+xorriso -as mkisofs -b limine-cd.bin \
+    -no-emul-boot -boot-load-size 4 -boot-info-table \
+    --efi-boot limine-eltorito-efi.bin \
+    -efi-boot-part --efi-boot-image --protective-msdos-label \
+    ${PRIVATE_DIR} -o ${OUTPUT}
+
+${LIMINE_DIR}/limine-install ${OUTPUT}
